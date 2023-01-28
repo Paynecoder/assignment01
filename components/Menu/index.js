7a5a6aa5c4c15ac59b03d65279aefe6ca94443c7
paynecoder <joshpayne03@icloud.com>
2023-01-27 16:17:37 -0800

    menu complete

--- components/Menu/index.js
+++ components/Menu/index.js
@@ -1,8 +1,8 @@
-import styles from "@/components/Menu/Menu.module.css";
 import Image from "next/Image";
 import hammy from "@/public/icons/menu-icon.png";
 import { useState } from "react";
 import menubg from "@/public/background-images/menuBackground.png";
+import styles from "@/components/Menu/Menu.module.css";
 
 export default function Menu() {
   const [isActive, setIsActive] = useState(true);
@@ -10,17 +10,44 @@ export default function Menu() {
 
   if (!isActive == true) {
     return (
-      <Image src={menubg} 
-      width={175} 
-      onClick={() => setIsActive(!isActive)}/>
+      <>
+        <div className={styles.menu}>
+          <div className={styles.menu__bgwrap}>
+            <Image src={menubg} className={styles.menu__bg} />
+          </div>
+          <div className={styles.menu__textwrap}>
+            <div className={styles.menu__textexit}>
+              <h1
+                className={styles.menu__exittext}
+                onClick={() => setIsActive(!isActive)}>
+            x</h1>
+            </div>
+            <div className={styles.menu__menutext}>
+              <ul className={styles.menu__menutext__list}>
+                <a href="http://localhost:3000/">
+                  <li>Home</li>
+                </a>
+                <a href="http://localhost:3000/about">
+                  <li>About</li>
+                </a>
+                <a href="http://localhost:3000/contact">
+                  <li>Contact</li>
+                </a>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </>
     );
   } else {
     return (
-      <Image
-        src={hammy}
-        style={{ width: "70px", height: "55px", cursor: "pointer" }}
-        onClick={() => setIsActive(!isActive)}
-      />
+      <>
+        <Image
+          src={hammy}
+          style={{ width: "70px", height: "55px", cursor: "pointer" }}
+          onClick={() => setIsActive(!isActive)}
+        />
+      </>
     );
   }
 }
