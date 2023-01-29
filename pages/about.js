abee896cc7a54bba2a3591ff3bed4f7d2b3a0843
paynecoder <joshpayne03@icloud.com>
2023-01-29 02:30:53 -0800

    working carousel, about page test 100%

--- pages/about.js
+++ pages/about.js
@@ -4,14 +4,37 @@ import Menu from "@/components/Menu/index.js";
 import Grad from "@/components/Grad/index.js";
 import Head from "next/head";
 import Image from "next/Image";
-import data from "@/data/data.json"
-import { useState, useEffect } from "react"
-import leftar from "@/public/icons/leftArrow.png"
-import rightar from "@/public/icons/rightArrow.png"
+import jsonData from "@/data/data.json";
+import { useState, useEffect } from "react";
+import leftar from "@/public/icons/leftArrow.png";
+import rightar from "@/public/icons/rightArrow.png";
+import upar from "@/public/icons/upwardArrow.png";
+import downar from "@/public/icons/down.png";
 
 export default function About() {
   const [SlideID, setSlideID] = useState(0);
-  const [caption, setCaption] = useState([]);
+  const [data, setData] = useState([]);
+  const caroimg = require(`@/public/carousel-images/${SlideID}.jpg`);
+
+  useEffect(() => {
+    setData(jsonData);
+  }, []);
+
+  const handleArrowClick = (direction) => {
+    if (direction === "left") {
+      if (SlideID === 0) {
+        setSlideID(data.length - 1);
+      } else {
+        setSlideID(SlideID - 1);
+      }
+    } else {
+      if (SlideID === data.length - 1) {
+        setSlideID(0);
+      } else {
+        setSlideID(SlideID + 1);
+      }
+    }
+  };
 
   return (
     <>
@@ -93,35 +116,88 @@ export default function About() {
               </p>
             </div>
           </div>
-          <div
-            id="carouselImage"
-            style={{
-              height: "200px",
-              backgroundRepeat: "no-repeat",
-              backgroundSize: "cover",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              margin: "40px 0px",
-              width: "500px",
-              outline: "1px solid black",
-            }}
-          >
-            <div className={styles.carouselcontent}>
-              <Image src={leftar} width={50}/>
-                <div
-                  id="testOnImageHere"
+          <div className={styles.content__carousel}>
+            <Image className={styles.content__carouselimage} src={caroimg} />
+            <div
+              id="carouselImage"
+              style={{
+                height: "12.5em",
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "cover",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                margin: "2.5em 0em",
+                width: "31.25em",
+              }}
+            >
+              <div className={styles.carouselcontent}>
+                <Image
+                  src={leftar}
                   style={{
-                    backgroundColor: "var(--quinary-color)",
-                    padding: "10px",
-                    margin: "0px 50px",
-                  }}>
-                  p
-                </div>
-              <Image src={rightar} width={50}/>
+                    zIndex: 1,
+                    width: "3.125em",
+                    height: "3.125em",
+                  }}
+                  onClick={() => handleArrowClick("left")}
+                />
+                {data.map((caption, index) => {
+                  if (index === SlideID) {
+                    return (
+                      <>
+                        <Image />
+                        <div
+                          key={index}
+                          id="testOnImageHere"
+                          style={{
+                            backgroundColor: "rgba(16, 46, 53, 0.5)",
+                            padding: "0.625em",
+                            margin: "0em 3.125em",
+                            zIndex: 1,
+                          }}
+                        >
+                          {index === SlideID && caption.value}
+                        </div>
+                      </>
+                    );
+                  }
+                })}
+                <Image
+                  src={rightar}
+                  style={{
+                    zIndex: 1,
+                    width: "3.125em",
+                    height: "3.125em",
+                  }}
+                  onClick={() => handleArrowClick("right")}
+                />
+              </div>
             </div>
           </div>
+
+          <div className={styles.content__departments}>
+            <div></div>
+            <h1>Departments</h1>
+            <div></div>
+          </div>
+
+          <div className={styles.content__departments__list}>
+            <ul>
+              <li>Applied & Natural Sciences</li>
+              <li>Business & Media</li>
+              <li>Computing & IT</li>
+              <li>Engineering</li>
+              <li>Health Sciences</li>
+              <li>Trades & Apprenticeships</li>
+            </ul>
+          </div>
         </div>
+        <a>
+          <span className={styles.arrowcontain}>
+            <Image src={upar} className={styles.arrowup} />
+            <Image src={downar} className={styles.downar} />
+          </span>
+        </a>
       </main>
     </>
   );
